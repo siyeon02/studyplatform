9290474d3da383c929e2f1c324a8325f5c86aac3
siyeon02 <chrysapark@cau.ac.kr>
2025-05-12 14:34:14 +0900

    기본 세팅

---
+++ src/main/java/com/project/studyplatform/StudyplatformApplication.java
@@ -0,0 +1,13 @@
+package com.project.studyplatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyplatformApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudyplatformApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/project/studyplatform/StudyplatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.studyplatform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyplatformApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
