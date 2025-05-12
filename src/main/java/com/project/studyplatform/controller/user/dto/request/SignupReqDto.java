d248262131c572e8692247dee8b493533f6fa3ce
siyeon02 <chrysapark@cau.ac.kr>
2025-05-12 21:44:48 +0900

    회원가입 dto

---
+++ src/main/java/com/project/studyplatform/controller/user/dto/request/SignupReqDto.java
@@ -0,0 +1,32 @@
+package com.project.studyplatform.controller.user.dto.request;
+
+import com.project.studyplatform.domain.user.Status;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.validator.constraints.Length;
+
+@Getter
+@NoArgsConstructor
+public class SignupReqDto {
+
+    @NotBlank(message = "이름을 입력하세요.")
+    @Length(max = 20, message = "이름은 최대 20자입니다.")
+    private String name;
+
+    @NotBlank(message = "이메일을 입력하세요.")
+    @Email(message = "올바른 이메일 형식을 입력하세요.")
+    private String email;
+
+    @NotBlank(message = "비밀번호를 입력하세요.")
+    private String password;
+
+    @NotBlank(message = "닉네임을 입력하세요.")
+    private String nickname;
+
+    @NotNull
+    private Status status;
+
+}
