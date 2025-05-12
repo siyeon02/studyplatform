69bbfe8b3912702c347e0471f6f2557a1491b854
siyeon02 <chrysapark@cau.ac.kr>
2025-05-12 21:43:54 +0900

    유저 레포지토리

---
+++ src/main/java/com/project/studyplatform/domain/user/repository/UserRepository.java
@@ -0,0 +1,12 @@
+package com.project.studyplatform.domain.user.repository;
+
+import com.project.studyplatform.domain.user.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    boolean existsByEmail(String email);
+    Optional<User> findByEmail(String email);
+
+}
