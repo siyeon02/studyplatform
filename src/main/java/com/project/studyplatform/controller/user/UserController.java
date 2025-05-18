a73d955821d00949a1c18b705cc6d95b2e640fdd
siyeon02 <chrysapark@cau.ac.kr>
2025-05-18 22:14:04 +0900

    유저 조회와 수정 컨트롤러

--- src/main/java/com/project/studyplatform/controller/user/UserController.java
+++ src/main/java/com/project/studyplatform/controller/user/UserController.java
@@ -1,30 +1,40 @@
 package com.project.studyplatform.controller.user;
 
-import com.project.studyplatform.controller.user.dto.request.LoginReqDto;
 import com.project.studyplatform.controller.user.dto.request.SignupReqDto;
-import com.project.studyplatform.controller.user.dto.response.LoginRespDto;
+import com.project.studyplatform.controller.user.dto.request.UserProfileEditReqDto;
+import com.project.studyplatform.controller.user.dto.response.UserProfileRespDto;
 import com.project.studyplatform.controller.user.dto.response.SignupRespDto;
+import com.project.studyplatform.domain.user.User;
 import com.project.studyplatform.service.UserService;
 import com.project.studyplatform.util.ApiResult;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/api/auth")
+@RequestMapping("/api")
 public class UserController {
 
     private final UserService userService;
 
-    @PostMapping("/signup")
+    @PostMapping("/auth/signup")
     public ResponseEntity<ApiResult<SignupRespDto>> signup(@Valid @RequestBody SignupReqDto dto) {
         return ResponseEntity.status(HttpStatus.CREATED).body(ApiResult.success(userService.signup(dto)));
+    }
+
+    @GetMapping("/users/profile")
+    public ResponseEntity<ApiResult<UserProfileRespDto>> userProfile(@AuthenticationPrincipal(expression = "user") User user) {
+        return ResponseEntity.status(HttpStatus.OK).body(ApiResult.success(userService.findUserProfile(user.getEmail())));
+    }
+
+    @PutMapping("/users/profile")
+    public ResponseEntity<ApiResult<Void>> editUser(@AuthenticationPrincipal(expression = "user") User user, @Valid @RequestBody UserProfileEditReqDto dto) {
+        userService.userProfileEdit(user.getId(), dto);
+        return ResponseEntity.status(HttpStatus.OK).body(ApiResult.success(null));
     }
 
 }
