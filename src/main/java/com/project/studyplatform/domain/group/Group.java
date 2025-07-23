24e4d1131b2df5300becafe3ebaa4d8af67222bb
siyeon02 <chrysapark@cau.ac.kr>
2025-07-23 12:33:27 +0900

    그룹 엔티티 생성 및 양방향 매핑

---
+++ src/main/java/com/project/studyplatform/domain/group/Group.java
@@ -0,0 +1,33 @@
+package com.project.studyplatform.domain.group;
+
+import com.project.studyplatform.domain.BaseEntity;
+import com.project.studyplatform.domain.groupmember.GroupMember;
+import com.project.studyplatform.domain.member.Member;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "groups")
+@Getter
+@NoArgsConstructor
+public class Group extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column
+    private String name;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "manager_id")
+    private Member manager;
+
+    @OneToMany(mappedBy = "group", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<GroupMember> groupMembers = new ArrayList<>();
+
+}
